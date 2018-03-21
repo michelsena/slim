3247ea2c8d718782232e14d771a336dab81605c8
unknown <xy_michels@yahoo.com.br>
2018-03-21 07:43:00 -0300

    Projeto com a primeira action e rota

--- app/Action/HomeAction.php
+++ app/Action/HomeAction.php
@@ -5,13 +5,13 @@
       private $container;
 
       function __construct($container)    {
-        $this->$container = $container;
+        $this->container = $container;
       }
 
       function index($request, $response){
         $vars['page'] = "home";
 
-          return $this->$container->view->render($response, 'template.phtml', $vars);
+          return $this->container->view->render($response, 'template.phtml', $vars);
         // $response = $this->$container->view->render($response, 'template.phtml', $vars);
          //return $response;
       }
